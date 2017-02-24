9e143c9efca62c5448eea7cf7e89461bb9f3ade7
mhio <37205532+mhio@users.noreply.github.com>
2017-02-24 12:52:33 +0000

    Initial commit pulling code from www
    add example code and readme

---
+++ examples/search.js
@@ -0,0 +1,55 @@
+// # Search Example
+
+const path = require('path')
+const { Docker, Page } = require('../')
+
+// Extend the Page class with custom methods
+
+class SearchPage extends Page {
+
+  search(engine, term){
+    if (!this[engine]) throw new Error(`No search engine ${engine}`)
+    return this[engine](term)
+  }
+
+  google(term){
+    return this.openUrl('https://www.google.com')
+      .then(()=> this.wait('#lst-ib'))
+      .then(()=> this.browser.setValue('#lst-ib', term))
+      .then(()=> this.browser.submitForm('#tsf'))
+      .then(()=> this.wait('#b_results', 5000))
+  }
+  
+  bing(term){
+    return this.openUrl('https://www.bing.com')
+      .then(()=> this.wait('#sb_form_q'))
+      .then(()=> this.browser.setValue('#sb_form_q', term))
+      .then(()=> this.browser.submitForm('#sb_form'))
+      .then(()=> this.wait('#b_results', 5000))
+  }
+ 
+}
+
+
+// Crete a class instance
+
+const lp = new SearchPage({
+  host: 'www.bing.com',
+  scheme: 'https',
+  remote_browser: 'firefox',
+  remote_port: 44446
+})
+
+// Use the custom `.search` method
+
+lp.search('bing','test').then(res => {
+  console.log('search', res)
+  console.log('title', lp.title())
+  return lp.screenShot('./search.png')
+})
+.then(res => console.log('ss', res))
+.catch(err => {
+ lp.screenShot('./error.png')
+ console.error(err)
+})
+
