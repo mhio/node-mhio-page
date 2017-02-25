805ffa25f95438eca64e5071873ac90cb5b39f0c
Matt Hoyle <code@deployable.co>
2017-02-25 23:23:27 +0000

    rename test labels

---
+++ test/unit_page_spec.js
@@ -0,0 +1,13 @@
+const { Page } = require('../')
+
+describe('Unit::page::Page', function(){
+
+  describe('Includes modules', function(){
+  
+    it('should have Page attached', function(){
+      expect( Page ).to.be.ok
+    })
+
+  })
+
+})
