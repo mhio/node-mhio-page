e6f1cf11ba8fe9c05cca19c1db43fcf371eeac91
Matt Hoyle <code@deployable.co>
2017-02-25 22:56:08 +0000

    rename to scoped package

--- test/unit_package_spec.js
+++ test/unit_package_spec.js
@@ -1,15 +1,15 @@
-const wdio_page = require('../')
+const page = require('../')
 
-describe('Unit::webdriverio-page', function(){
+describe('Unit::deployable::page', function(){
 
   describe('Includes modules', function(){
   
     it('should have Page attached', function(){
-      expect( wdio_page.Page ).to.be.ok
+      expect( page.Page ).to.be.ok
     })
 
     it('should have Docker attached', function(){
-      expect( wdio_page.Docker ).to.be.ok
+      expect( page.Docker ).to.be.ok
     })
 
   })
