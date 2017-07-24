e7ecd89245a2e3af343ef46c9314b065aebfe9c9
mhio <37205532+mhio@users.noreply.github.com>
2017-07-24 04:02:11 +0000

    help and docker pulls
    Add chrome/firefox pulls to build

--- make.sh
+++ make.sh
@@ -4,20 +4,6 @@ set -ue
 
 rundir=$(cd -P -- "$(dirname -- "$0")" && printf '%s\n' "$(pwd -P)")
 canonical="$rundir/$(basename -- "$0")"
-cd "$rundir"
-
-build_test_docker(){
-  cd test/fixture/selenium-stub
-  docker build -t dply/selenium-standalone-stub:latest . 
-  docker tag dply/selenium-standalone-stub:latest dply/selenium-standalone-stub:chrome
-  docker tag dply/selenium-standalone-stub:latest dply/selenium-standalone-stub:firefox
-}
-
-
-build(){
-  build_test_docker
-}
-
 
 if [ -n "${1:-}" ]; then
   cmd=$1
@@ -26,5 +12,31 @@ else
   cmd=build
 fi
 
-$cmd "$@"
+cd "$rundir"
+
+build_test_docker(){
+  docker pull selenium/standalone-chrome
+  docker pull selenium/standalone-firefox
+  cd test/fixture/selenium-stub
+  docker build -t dply/selenium-standalone-stub:latest . 
+  docker tag dply/selenium-standalone-stub:latest dply/selenium-standalone-stub:chrome
+  docker tag dply/selenium-standalone-stub:latest dply/selenium-standalone-stub:firefox
+}
+
+
+run_help(){
+  echo "Commands:"
+  awk '/  ".*"/{ print "  "substr($1,2,length($1)-3) }' make.sh
+}
+set +x
+case $cmd in
+  "build")                  build_test_docker "$@";;
+  "build:test")             build_test_docker "$@";;
+  "build:test:docker")      build_test_docker "$@";;
+
+  "watch")                  run_watch "$@";;
+
+  '-h'|'--help'|'h'|'help') run_help;;
+  *)                        $cmd "$@";;
+esac
 
