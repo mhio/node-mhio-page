8f1b48f60be8dabe55002bed86706aa47a67bff8
Matt Hoyle <code@deployable.co>
2018-02-23 06:58:23 +0000

    Update dev deps
    chai: "^4.1.2"
    chai-as-promised: "^7.1.1"
    chai-fs: "^2.0.0"
    mocha: "^5.0.1"
    
    chai-as-promised needs to be loaded last of the plugins to work!!!

--- test/fixture/mocha-setup.js
+++ test/fixture/mocha-setup.js
@@ -2,8 +2,8 @@
 global.chai = require('chai')
 //global.sinon = require('sinon')
 global.expect = chai.expect
-chai.use(require('chai-as-promised'))
 chai.use(require('chai-fs'))
+chai.use(require('chai-as-promised'))
 
 require('bluebird').config({
   longStackTraces: true,
