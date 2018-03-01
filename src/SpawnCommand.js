f3d38b1c76d14aac560dff7dc8cc01a2b21294db
mhio <37205532+mhio@users.noreply.github.com>
2018-03-01 07:09:01 +0000

    Fix some coverage

--- src/SpawnCommand.js
+++ src/SpawnCommand.js
@@ -4,7 +4,8 @@ const { spawn }= require('child_process')
 
 
 class SpawnCommand {
-  // Run a generic `docker` passing along all args.
+
+  // Run a command, catch its logs, resolve the promise when it exits
   static run( command, args, ){ //options = {} ){
     return new Promise((resolve, reject) => {
       debug('SpawnCommand run command: "%s" %j', command, args)
@@ -51,6 +52,46 @@ class SpawnCommand {
 
     })
   }
+
+  // Run a command, catch its logs
+  // static runBackground( command, args, options = {} ){
+  //   debug('SpawnCommand runBackground command: "%s" %j', command, args)
+
+  //   let dkr = spawn(command, args)
+
+  //   let results = {
+  //     errors: [],     // Any errors picked up
+  //     stdout: [],     // stdout
+  //     stdout_buffer: [],     // stdout
+  //     stderr: [],     // stderr
+  //     stderr_buffer: [],     // stderr
+  //     exit_code: null // process.exit code
+  //   }
+
+  //   dkr.stdout.on('data', (data) => {
+  //     results.stdout_buffer.push(data)
+  //     if (options.stdout_cb) options.stdout_cb(data)
+  //   })
+
+  //   dkr.stderr.on('data', (data) => {
+  //     results.stderr_buffer.push(data)
+  //     if (options.stderr_cb) options.stderr_cb(data)
+  //   })
+
+  //   dkr.on('close', (exit_code) => {
+  //     debug(`SpawnCommand "${command}" finished with ${exit_code}`)
+  //     results.exit_code = exit_code
+
+  //     if ( exit_code !== 0 ) {
+  //       let err = new Error(`SpawnCommand "${command}" exited with "${exit_code}" for: docker ${args.join(' ')}:\n${results.stderr[0]}`)
+  //       err.results = results
+  //       if (options.cb) options.cb(err)
+  //     }
+
+  //     if (options.cb) options.cb(null, results)
+  //   })
+  // }
+
 }
 
 module.exports = { SpawnCommand }
